abf08d854abe3645989a271742e954057357fe49
toxashark <toxashark@gmail.com>
2022-01-16 21:59:04 +0200

    add burger menu

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,5 +1,33 @@
+try {
+    function Burger() {
+        const burger = document.querySelector('.burger')
+        const crossBurger = document.querySelector('.cross_burger')
+        const menuMobile = document.querySelector('.menu-mobile')
+        const catalogMobile = document.querySelector('.catalog_mobile')
+        const catalogListMenu = document.querySelector('.catalog_list_menu')
 
+        burger.addEventListener('click', function() {
+            this.classList.add('hide')
+            crossBurger.classList.remove('hide')
+            menuMobile.classList.add('open_menu')
+        })
 
+        crossBurger.addEventListener('click', function() {
+            burger.classList.remove('hide')
+            menuMobile.classList.remove('open_menu')
+            this.classList.add('hide')
+            catalogListMenu.classList.add('hide')
+        })
+
+        catalogMobile.addEventListener('click' , function () {
+            catalogListMenu.classList.toggle('hide')
+        })
+    }
+} catch (e) {
+    console.log(e.message)
+}
+
+Burger()
 const swiper = new Swiper('.swiper', {
     slidesPerView: 4,
     initialSlide:0,
@@ -58,11 +86,3 @@ const slider__reviews = new Swiper(".slider__reviews", {
         clickable: true,
     },
 });
-
-
-
-window.addEventListener('resize', function(e) {
-    if(e.target.innerWidth < 501)
-    console.log(e.target.innerWidth)
-
-})
